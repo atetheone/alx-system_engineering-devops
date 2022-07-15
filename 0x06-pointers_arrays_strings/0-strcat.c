55e863dbb64a827450dbc0e8c3a1c9c24fb9304c
atetheone <atevirran@gmail.com>
2022-07-14 18:24:47 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenationf t<o strings
+ * @dest: first strng, the destination
+ * @src: second string, the sours*Return: dest = dest + src
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int  = 0; l2 = 0;
